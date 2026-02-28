741166a7f236b36f777206e0f412e36dc43875c3
Your Name <you@example.com>
2026-02-28 07:22:08 +0100

    Function array_sort_numerically(a) is now returning sorted slice

--- js/array/array_sort_numerically.js
+++ js/array/array_sort_numerically.js
@@ -6,7 +6,7 @@ function array_sort_numerically(aRealArray) {
     // Return array of numbers sorted by numbers ascending
     assert_arguments_length(arguments, 1, 1, 'array_sort_numerically(real_array)');
     assert_array(aRealArray, 'array', 'array_sort_numerically(real_array)');
-    return aRealArray.sort((a, b) => a - b);
+    return aRealArray.slice(0).sort((a, b) => a - b);
 }
 
 globalThis.exports = {array_sort_numerically};
